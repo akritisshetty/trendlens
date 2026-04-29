d88a2b86cf559eff6a59970ffd8d6eec00e18828
Akriti S Shetty <akritishetty.in@gmail.com>
2026-04-29 22:54:24 +0530

    Update convert_npy_to_csv.py

--- convert_npy_to_csv.py
+++ convert_npy_to_csv.py
@@ -1,21 +1,21 @@
 import os
 import numpy as np
 
-# CLIP Embeddings
+# ── CLIP Embeddings ────────────────────────────────────────────────────────────
 npy_one_file_path = "trendlens_outputs/embeddings.npy"
 
 if os.path.exists(npy_one_file_path):
     data = np.load(npy_one_file_path)
     np.savetxt("trendlens_outputs/embeddings.csv", data, delimiter=",")
 
-# UMAP 2d
+# ── UMAP 2d ────────────────────────────────────────────────────────────
 npy_two_file_path = "trendlens_outputs/umap_2d.npy"
 
 if os.path.exists(npy_two_file_path):
     data = np.load(npy_two_file_path)
     np.savetxt("trendlens_outputs/umap_2d.csv", data, delimiter=",")
 
-# UMAP 10d    
+# ── UMAP 10d ──────────────────────────────────────────────────────────── 
 npy_three_file_path = "trendlens_outputs/umap_10d.npy"
 
 if os.path.exists(npy_three_file_path):
